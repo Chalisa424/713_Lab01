ca33eea010536fd84e17914286bece8b70cee9ff
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-02 01:45:53 +0700

    Lab01_Task7

---
+++ for.js
@@ -0,0 +1,8 @@
+let names = ['GojoShatoru','Yuji','Nobara','Megumi','Panda']
+for(let i = 0; i < names.length; i++){
+    if (names[i].length > 6){
+        console.log("สวัสดีคุณ " + names[i])
+    } else{
+        console.log("Hello " + names[i])
+    }
+}
