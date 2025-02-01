7d796db0f02141fc4d8e4a079aeac17d79a28bc0
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-01 22:19:06 +0700

    Lab01_Task6_01

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-console.log('Hello World');
-
-let x =5
-let y =10
-
-console.log(x+y)
+let x = 5
+let y = 10
+if (x > 5 && y > 12){
+    console.log('Very high value')
+}else{
+    console.log(x * y + x / y)   
+    }
